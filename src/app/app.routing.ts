71db0abc057db2b48d5ee944c24d87c09fd5fe9c
gsferoz <gsferoz@gmail.com>
2025-07-25 02:21:32 -0400

    updated images

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,20 +3,20 @@ import { CommonModule, } from '@angular/common';
 import { BrowserModule  } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
-import { ComponentsComponent } from './components/components.component';
-import { ProfileComponent } from './examples/profile/profile.component';
-import { SignupComponent } from './examples/signup/signup.component';
+// import { ComponentsComponent } from './components/components.component';
+// import { ProfileComponent } from './examples/profile/profile.component';
+// import { SignupComponent } from './examples/signup/signup.component';
 import { LandingComponent } from './examples/landing/landing.component';
-import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';
+// import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 const routes: Routes =[
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'landing',             component: ComponentsComponent },
-    { path: 'user-profile',     component: ProfileComponent },
-    { path: 'signup',           component: SignupComponent },
+    // { path: 'landing',             component: ComponentsComponent },
+    // { path: 'user-profile',     component: ProfileComponent },
+    // { path: 'signup',           component: SignupComponent },
     { path: 'home',          component: LandingComponent },
-    { path: 'nucleoicons',      component: NucleoiconsComponent }
+    // { path: 'nucleoicons',      component: NucleoiconsComponent }
 ];
 
 @NgModule({
